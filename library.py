ef62ea0b3912690411592ad08e1d935693c8eb69
POTHURU-JASWANTH <pothuru.jaswanth2023@vitstudent.ac.in>
2026-02-07 11:51:25 +0530

    Added student registration module

--- library.py
+++ library.py
@@ -1,1 +1,7 @@
 print("Library Management System Initialized")
+
+def register_student(name, roll):
+    print("Registering Student")
+    print("Name:", name)
+    print("Roll No:", roll)
+    print("Registration Successful")
