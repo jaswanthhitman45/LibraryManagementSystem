45abf55d7f4e8fa19818076e79081acde1c9d9b4
POTHURU-JASWANTH <pothuru.jaswanth2023@vitstudent.ac.in>
2026-02-07 12:14:36 +0530

    Updated issue book logic

--- library.py
+++ library.py
@@ -1,14 +1,2 @@
-print("Library Management System Initialized")
-
-def register_student(name, roll):
-    print("Registering Student")
-    print("Name:", name)
-    print("Roll No:", roll)
-    print("Registration Successful")
-
-def login(username, password):
-    print("Checking credentials")
-    if username == "admin" and password == "admin123":
-        print("Login Successful")
-    else:
-        print("Invalid Login")
+def issue_book(book_id):
+    print("Book issued")
